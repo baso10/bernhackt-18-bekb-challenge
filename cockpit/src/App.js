c75f538604f4bfd789b97c1c15fba715e03683ca
Sandro <sandro-portner@hotmail.com>
2018-08-25 12:40:39 +0200

    login and search

--- cockpit/src/App.js
+++ cockpit/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './Authentification/Login';
+import Orderscreen from './Orderscreen';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      token:null,
+      loginRequired: false
+    }
+  }
+
+  componentWillMount(){
+    
+  }
+
   render() {
+    const loginscreen = (
+      <Login appContext={this} />
+    )
+
+    const orderscreen = (
+      <Orderscreen appContext={this}/>
+    )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loginRequired && this.state.token ? orderscreen : loginscreen}
       </div>
     );
   }
 }
+
+const style = {
+  margin: 15,
+};
 
 export default App;
