4570bc1b768100c72327db3678b6aaea8cfb681c
Sandro <sandro-portner@hotmail.com>
2018-08-25 14:03:19 +0200

    bugfixes

--- cockpit/src/Authentification/Login.js
+++ cockpit/src/Authentification/Login.js
@@ -33,29 +33,6 @@ class Login extends Component {
     }
   };
 
-  /*
-    axios.post(apiBaseUrl+'login', payload)
-    .then(function (response) {
-    console.log(response);
-    if(response.data.code == 200){
-    console.log("Login successfull");
-    var uploadScreen=[];
-    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-    }
-    else if(response.data.code == 204){
-    console.log("Username password do not match");
-    alert("username password do not match")
-    }
-    else{
-    console.log("Username does not exists");
-    alert("Username does not exist");
-    }
-    })
-    .catch(function (error) {
-    console.log(error);
-    });*/
-
   render() {
     return (
       <div className="flex-container">
@@ -63,6 +40,8 @@ class Login extends Component {
           <Card className="row">
             <CardContent className="flex-item">
               <img src={Logo} alt="Logo" style={{ width: "35%" }} />
+              <h2>BÄNZin</h2>
+
               <div onKeyDown={this.handleKeyPress}>
                 <TextField
                   type="text"
